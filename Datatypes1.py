e4671f8b75cb96152c471f5a50fa9ce0bcaaf6f9
abhijitgavale2004 <128979169+abhijitgavale2004@users.noreply.github.com>
2026-08-26 08:09:29 +0530

    Day 2 - Multiple Data Types

---
+++ Datatypes1.py
@@ -0,0 +1,12 @@
+age=22
+height=5.5
+name="Abhijit"
+is_student=True
+print("Age:-",age)
+print("height:-",height)
+print("Name:-",name)
+print("IS student:-",is_student)
+print("Datatype:-",type(age))
+print("Datatype:-",type(height))
+print("Datatype:-",type(name))
+print("Datatype:-",type(is_student))
