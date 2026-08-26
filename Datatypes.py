21ab4d3b5311bd3e01fc57cf0bddf0d7478f35c9
abhijitgavale2004 <128979169+abhijitgavale2004@users.noreply.github.com>
2026-08-26 08:00:44 +0530

    Day 2- DataType

---
+++ Datatypes.py
@@ -0,0 +1,20 @@
+#Datatypes..
+Age=22
+print("Age:",Age)
+print("Data type:-",type(Age))
+  #2nd example
+Marks=85.2
+print("Marks:",Marks)
+print("Data type:-",type(Marks))
+#3rd example
+name="Rahul"
+print("Name:-",name)
+print("Data type:-",type(name))
+#4th example
+is_student = True 
+print("IS student:",is_student )
+print("Data type:-",type(is_student))
+#5th eample
+number=3+4j
+print("complex number:-",number)
+print("Data typee:-",type(number))
