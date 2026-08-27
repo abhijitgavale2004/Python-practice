aeaf76788c1e00ded5d76137db8400c596e2ccff
abhijitgavale2004 <128979169+abhijitgavale2004@users.noreply.github.com>
2026-08-27 08:48:40 +0530

    Day 3 - Operators

---
+++ Arithmetic operator.py
@@ -0,0 +1,9 @@
+a=20
+b=10
+print("Addition=",a+b)
+print("Subtraction=",a-b)
+print("Multiplication=",a*b)
+print("Division=",a/b)
+print("modulus=",a%b)
+print("Power=",a**b)
+print("Floor Division=",a//b)
